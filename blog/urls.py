f03b73fc918c9cf32e354e600170fb42ffc00f6a
Hasan Choubineh <hasanch80@gmail.com>
2024-10-29 11:26:21 +0330

    create detail view with templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 
-    path('posts/', views.posts_list_view, name='posts_list')
+    path('posts/', views.posts_list_view, name='posts_list'),
+    path('posts/<int:pk>/', views.post_detail_view, name= 'post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import *
+from django.shortcuts import get_object_or_404
 
 def posts_list_view(request):
     
     posts = Post.objects.all()
     return render(request, 'blog/posts_list.html', context={'posts' : posts})
+
+
+def post_detail_view(request,pk):
+
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', context={'post' : post})
