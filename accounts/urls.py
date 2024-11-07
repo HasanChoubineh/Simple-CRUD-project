a28cf271988e7147f1befea69c4092cceef6e87f
Hasan Choubineh <hasanch80@gmail.com>
2024-11-07 19:38:07 +0330

    implement signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import views as auth_views 
 from django.urls import path 
-
+from . import views 
 
 urlpatterns = [
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
